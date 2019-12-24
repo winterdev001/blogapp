90cb5cded10517fed68d27e952a8312a87ddcff2
nadinantire <nadinantire@gmail.com>
2019-12-24 14:03:38 +0200

    errorpages added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -100,7 +100,6 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
     end
